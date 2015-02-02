1c7365c7dbac83319bf2332d58cb84df8b72db44
shaon <shaon.ashfak@gmail.com>
2015-02-03 00:59:40 +0600

    2nd Commit

--- src/user/Meal.java
+++ src/user/Meal.java
@@ -26,7 +26,8 @@ public class Meal extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.sendRedirect("/user/InputMeal.jsp");
+
 	}
 
 	/**
